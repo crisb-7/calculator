e3cff6c92ead4a77e7a15199eeef7089e4a73ef7
crisb-7 <cristofer.becerra@protonmail.com>
2023-04-19 10:50:53 -0600

    Limit number of digits typed in display. Working on expanding display.

--- script.js
+++ script.js
@@ -33,6 +33,9 @@ function runEquals() {
     num2 = Number(displayNum2);
     console.log(num1, num2, operator)
     result = operate(operator, num1, num2)
+    if (result-parseInt(result) != 0) {
+        result = result.toFixed(6)
+    }
     display.innerHTML = result;
     displayNum1 = String(result);
     displayNum2 = "";
@@ -40,7 +43,7 @@ function runEquals() {
 }
 
 function limitDecimals(number) {
-    let limit = 5;
+    let limit = 6;
     if (String(number).length > limit) {
         return Number(number.toFixed(limit));
     } else {
@@ -72,6 +75,9 @@ const numbers = document.querySelectorAll(".numbers button")
 numbers.forEach((button) => {
     button.addEventListener("click", () => {
         if (displayOperator.length == 0) {
+            if (String(displayNum1).length >= 5) {
+                return;
+            }
             if (String(displayNum1).includes(".0") || String(displayNum1).includes(`${Number(displayNum1)}.`)) {
                 displayNum1 += button.id;
             } else {
@@ -81,6 +87,9 @@ numbers.forEach((button) => {
             }
             display.innerHTML = displayText(displayNum1, displayNum2, displayOperator);
         } else {
+            if (String(displayNum2).length >= 7) {
+                return;
+            }
             if (String(displayNum2).includes(".0") || String(displayNum2).includes(`${Number(displayNum2)}.`)) {
                 displayNum2 += button.id;
             } else {
